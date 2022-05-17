ca59b00f997632bc4070c58a7512a4a47cd13e4f
martinsejas <martinhugosejas@gmail.com>
2022-05-16 23:07:49 -0300

    termino de pagina de login, sem request

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,11 +1,159 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import { goToHome, goToSignUp } from '../../routes/cordinator';
+import * as Yup from 'yup'; 
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { GreyBorderTextField, LoginPageContentDiv, LoginPageFormDiv, LoginPageLogoDiv, LoginPageMainDiv } from './styled';
+import { Button, InputAdornment, TextField } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
+import IconButton from '@mui/material/IconButton';
+
 
 const LoginPage = () => {
+    const navigate = useNavigate(); 
+    const [showPassword, setShowPassword] = useState(false); 
+
+    useEffect( ()=>{
+        if(window.localStorage.length > 0)
+        {
+            goToHome(navigate); 
+        }
+
+    },[])
+
+
+    
 
     return (
-        <>
-            LoginPage
-        </>
+        <LoginPageMainDiv>
+
+            <LoginPageLogoDiv>
+                <img alt='FutureEats Logo' src={logo}/> 
+            </LoginPageLogoDiv>
+
+            <LoginPageContentDiv>
+                <p>Entrar</p>
+
+
+                <LoginPageFormDiv>
+
+                <Formik
+                initialValues={{email: "", password: ""}}
+
+                validationSchema = {Yup.object({
+                    email: Yup
+                    .string('Insira o seu email')
+                    .email('Entre um email válido')
+                    .required('Campo obrigátorio'), 
+                    password: Yup
+                    .string('Insira a sua senha')
+                    .min(6, 'Mínimo 6 caracteres')
+                    .max(25, 'Máximo 25 caaracteres')
+                    .required('Campo obrigátorio')
+                })}
+
+                onSubmit = { (values, actions) => {
+                    //codigo de submeter importante
+                    let body = {
+                        email: values.email, 
+                        password: values.password
+                    }
+
+                    alert(body); 
+                }}
+
+                >
+                    { (props) => {
+                        return (
+                            <Form>
+                               <Field name ="email">
+                                   {({field, form}) => (
+                                       <GreyBorderTextField
+                                       {...field}
+                                       fullWidth
+                                       label = "E-mail*"
+                                       variant = 'outlined'
+                                       placeholder='email@email.com'
+                                       type='email'
+                                       focused
+                                       value={form.values.email}
+                                       onChange={form.handleChange}
+                                       error={form.touched.email && Boolean(form.errors.email)}
+                                       helperText={form.touched.email && form.errors.email}
+                                       />
+                                   )}
+                               </Field>
+
+                               <Field name ="password">
+                                   {({field, form}) => (
+                                       <GreyBorderTextField
+                                       {...field}
+                                       fullWidth
+                                     
+                                       sx={{
+                                           marginTop: '16px',
+                                           colorScheme: '#b8b8b8 ',
+                                          
+                                       }}
+                                       label = "Senha*"
+                                       variant = 'outlined'
+                                       placeholder='Mínimo 6 caracteres '
+                                       focused
+                                       type={showPassword ? 'text':'password'}
+                                       value={form.values.password}
+                                       onChange={form.handleChange}
+                                       error={form.touched.password && Boolean(form.errors.password)}
+                                       helperText={form.touched.password && form.errors.password}
+                                       InputProps={ {  endAdornment: 
+                                        <InputAdornment position = "end" >
+                                            <IconButton
+                                            aria-label='toggle password visibility'
+                                            onClick={()=> setShowPassword(!showPassword)}
+                                            edge = 'end'
+                                            
+                                            >
+                                                {showPassword ? <VisibilityOff color='#000000'/>: <Visibility color='#000000'/>}
+                                            </IconButton>
+
+                                        </InputAdornment>,
+                                       }}
+                                       />
+                                   )}
+                               </Field>
+                               <Button 
+                               variant='contained'
+                                fullWidth
+                                disableElevation
+                                
+                                sx={{
+                                    marginTop: '16px',
+                                    colorScheme: '#b8b8b8 ',
+                                    borderRadius: '0px',
+                                    backgroundColor: '#5cb646',
+                                    fontFamily: "'Roboto', sans-serif",
+                                    textTransform: "none",
+                                    textDecorationColor: '#000000',
+                                    color: '#000000'
+                                }}
+                                >Entrar</Button>
+                            </Form>
+                        )
+                    }}
+                    
+                </Formik>
+       
+                </LoginPageFormDiv>
+
+
+            
+
+
+            <p onClick={()=> goToSignUp(navigate)}>Não possui cadastro? Clique aqui.</p>
+            </LoginPageContentDiv>
+           
+        </LoginPageMainDiv>
     )
 }
 
--- src/pages/login/styled.js
+++ src/pages/login/styled.js
@@ -1,1 +1,84 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { TextFields } from "@mui/icons-material"; 
+import { Button, InputAdornment, TextField } from '@mui/material';
+
+
+export const LoginPageMainDiv = styled.div`
+display: grid; 
+grid-template-rows: 146px 1fr; 
+font-family: 'Roboto', sans-serif; 
+font-weight: 500; 
+font-size: 16px; 
+letter-spacing: -0.39px;
+color: black; 
+
+`
+
+export const LoginPageLogoDiv = styled.div`
+display: flex; 
+flex-direction: column; 
+
+align-items: center; 
+justify-content: flex-end; 
+
+img{
+    max-width: 104px; 
+    max-height: 58px; 
+}
+
+`
+
+export const LoginPageContentDiv = styled.div`
+display: flex; 
+flex-direction: column; 
+align-items: center; 
+
+margin-top: 28px; 
+
+p{
+    margin-top: 28px; 
+}
+
+`
+
+export const LoginPageFormDiv = styled.div`
+display: flex; 
+flex-direction: column; 
+width: 90%; 
+
+#passwordInput{
+    margin-top: 16px; 
+}
+
+Button{
+    color: #5cb646;
+
+}
+
+.MuiSvgIcon-root {
+    color: #b8b8b8;
+}
+
+
+    .MuiButtonBase-root {
+    color: #000000;
+}
+
+`
+
+
+export const GreyBorderTextField = styled(TextField)`
+  & label.Mui-focused {
+    color:  #b8b8b8 ;
+    border: 1px;
+  }
+  & .MuiOutlinedInput-root {
+    &.Mui-focused fieldset {
+      border-color: #b8b8b8 ;
+    }
+  }
+
+  & .MuiIconButton-root{
+      color: 'black'
+  }
+`;
